59d161c815993bea5d3b7ac38172d8a41d7e53d5
Jesica Quero <jesi.qero@gmail.com>
2023-02-23 19:57:40 -0300

    Primera escritura en el repositorio

---
+++ CV.js
@@ -0,0 +1,17 @@
+
+/**
+document.getElementById('boton').addEventListener('click', function () {
+    console.log("creamos");
+    document.getElementById("prueba").innerHTML = "hhhhhhhhhhhhhh";
+});
+
+document.getElementById('boton-color').addEventListener('click', function () {
+    document.body.style.backgroundColor = '#2EAB07';
+})
+ 
+
+document.getElementById('boton').onclick=function(){
+    console.log("a ver")
+    document.getElementById("prueba").innerHTML = "Hello JavaScript!"
+}
+*/
